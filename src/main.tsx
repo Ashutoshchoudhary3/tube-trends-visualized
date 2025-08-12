9c41b2fe27fc6ab77c309e072043c6fef2fc2c0f
Ashutosh <ajaat8209@gmail.com>
2025-08-12 13:38:20 +0530

    Configure tube-trends for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- Import this
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* Add the router here */}
+    <BrowserRouter basename="/tube-trends-visualized/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
